b4880dd13f50fede76a3fc3c764e696634090c4a
Fritty96 <mphillips39@mail.csuchico.edu>
2017-09-27 16:47:53 -0700

    Final Vector Grocery

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main()
+{
+  vector <string> list; 
+  int numItems = 0;
+  string Item;
+  char input;
+  
+do{
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item"; 
+  cout<<"\n (S)how grocery list";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/S/Q): ";
+  cin>>input ;
+
+    if (input == 'a' || input == 'A'){
+         
+        cout << "What is the name of the item?" << endl;
+        cin >> Item; 
+        list.push_back(Item);
+       
+        numItems++;
+
+                  
+    }  
+        else if(input == 's' || input == 'S') {
+            for (int a = 0; a < numItems; a++) {
+            cout << list[a] <<endl;
+            }
+
+        }
+      
+        
+
+}
+
+while (input != 'q' && input != 'Q' );
+
+
+return 0;
+}
